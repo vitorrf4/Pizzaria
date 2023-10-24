fc6d12a7ab8b656a6ad315437160a0501e2a2055
vitorrf4 <vitorrf4@gmail.com>
2023-10-24 00:04:30 -0300

    arruma estrutura de pastas

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ClienteComponent} from "./components/cliente/cliente.component";
+
+const routes: Routes = [
+  {path: "clientes", component: ClienteComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Frontend/src/app/components/acompanhamento/acompanhamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acompanhamento',
+  templateUrl: './acompanhamento.component.html',
+  styleUrls: ['./acompanhamento.component.css']
+})
+export class AcompanhamentoComponent {
+  constructor() {
+  }
+}
