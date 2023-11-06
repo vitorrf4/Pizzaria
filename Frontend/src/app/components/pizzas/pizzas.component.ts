5b1bccf1fe9ea3e11bd6ff28c6672b8d9fcc505e
vitorrf4 <vitorrf4@gmail.com>
2023-11-06 15:58:20 -0300

    refatora pedidos de pizza no angular

--- Frontend/src/app/components/pizzas/pizzas.component.ts
+++ Frontend/src/app/components/pizzas/pizzas.component.ts
@@ -1,9 +1,8 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {Sabor} from "../../models/Sabor";
 import {SaborService} from "../../services/sabor.service";
 import {Tamanho} from "../../models/Tamanho";
 import {TamanhoService} from "../../services/tamanho.service";
-import {PedidoFinal} from "../../models/PedidoFinal";
 import {PizzaPedido} from "../../models/PizzaPedido";
 import {CarrinhoService} from "../../services/carrinho.service";
 
@@ -13,40 +12,68 @@ import {CarrinhoService} from "../../services/carrinho.service";
   styleUrls: ['./pizzas.component.css']
 })
 export class PizzasComponent {
-  sabores: Sabor[] = [];
-  tamanhos : Tamanho[] = [];
-  tamanho  = new Tamanho();
+  saboresDB: Sabor[] = [];
+  tamanhosDB : Tamanho[] = [];
+  tamanho = new Tamanho();
   saboresSelecionados : Sabor[] = [];
+  maxQntdSabores : number[] = [];
+  quantidadeSabores = 1;
+  pizza! : PizzaPedido;
 
   constructor(private saborService: SaborService,
               private tamanhoService: TamanhoService,
               private carrinhoService: CarrinhoService) {
     this.saborService.listar().subscribe(resposta => {
-      this.sabores = resposta;
+      this.saboresDB = resposta;
     });
 
     this.tamanhoService.listar().subscribe(resposta => {
-      this.tamanhos = resposta;
-      this.ordernarTamanhoPorFatias();
+      this.tamanhosDB = resposta;
+      this.ordernarTamanhosPorQntdFatias();
     });
   }
 
+  construirPizza() {
+    this.pizza = new PizzaPedido(this.saboresSelecionados, this.tamanho, 1);
+  }
+
+  getMaxSabores() {
+    this.maxQntdSabores = [];
+
+    for (let i = 1; i <= this.tamanho.maxSabores; i++) {
+      this.maxQntdSabores.push(i);
+    }
+  }
+
   adicionarSabor(sabor: Sabor) {
-    if (this.saboresSelecionados.length >= this.tamanho.maxSabores) {
+    if (this.saboresSelecionados.length >= this.quantidadeSabores) {
       alert("Maximo de sabores antigido");
+      return;
+    }
+
+    if (this.saboresSelecionados.includes(sabor)) {
+      alert(`${sabor.nome} ja foi selecionado`);
       return;
     }
 
     this.saboresSelecionados.push(sabor);
   }
 
-  adicionarAoCarrinho() {
-    const pizzas = new PizzaPedido(this.saboresSelecionados, this.tamanho, 1);
-    this.carrinhoService.adicionarNoCarrinho(pizzas);
+  removerSabor(index: number) {
+    this.pizza.sabores.splice(index, 1);
   }
 
-  ordernarTamanhoPorFatias() {
+  adicionarAoCarrinho() {
+    if (this.pizza.sabores.length == 0) {
+      alert("Pedido inválido");
+      return;
+    }
+
+    this.carrinhoService.adicionarNoCarrinho(this.pizza);
+  }
+
+  ordernarTamanhosPorQntdFatias() {
     // funcao que orderna os tamanhos pela quantidade de fatias de forma ascendente
-    this.tamanhos.sort((tamanhoA, tamanhoB) => tamanhoA.qntdFatias - tamanhoB.qntdFatias)
+    this.tamanhosDB.sort((tamanhoA, tamanhoB) => tamanhoA.qntdFatias - tamanhoB.qntdFatias)
   }
 }
