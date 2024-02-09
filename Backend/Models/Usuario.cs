2aa7d949afaec566096bce2ba7562e45e65eec31
vitorrf4 <vitorrf4@gmail.com>
2024-02-09 12:56:13 -0300

    muda ordem de serializacao do json

--- Backend/Models/Usuario.cs
+++ Backend/Models/Usuario.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace pizzaria;
 
@@ -7,8 +8,11 @@ public class Usuario
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [JsonPropertyOrder(-3)]
     public int Id { get; set; }
+    [JsonPropertyOrder(-2)]
     public string Email { get; set; } = "";
+    [JsonPropertyOrder(-1)]
     public string Senha { get; set; } = "";
 
     public Usuario() { }
