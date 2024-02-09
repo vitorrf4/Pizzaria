ce3836f7e4abf64a7b2fcfcd7c1967280b8979ec
vitorrf4 <vitorrf4@gmail.com>
2024-02-09 18:59:53 -0300

    muda dto pra record

--- Backend/DTOs/LoginDTO.cs
+++ Backend/DTOs/LoginDTO.cs
@@ -1,13 +1,3 @@
 namespace pizzaria;
 
-public class LoginDTO
-{
-    public string Email { get; set; }
-    public string Senha { get; set; }
-
-    public LoginDTO(string email, string senha)
-    {
-        Email = email;
-        Senha = senha;
-    }
-}
+public record LoginDTO(string Email, string Senha) { }
